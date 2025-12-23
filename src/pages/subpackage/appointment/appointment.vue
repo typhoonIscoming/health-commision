856a3de65921226daad032643d88094fcc8e1913
Hang Xie <xiehang@mail.zjsk.cc>
2025-12-23 18:07:04 +0800

    添加服务时间

--- src/pages/subpackage/appointment/appointment.vue
+++ src/pages/subpackage/appointment/appointment.vue
@@ -61,6 +61,24 @@
 						<uv-icon name="arrow-right"></uv-icon>
 					</template>
 				</uv-form-item>
+				<uv-form-item
+					label="选择预约时间"
+					prop="date"
+					borderBottom
+					labelWidth="auto"
+					@click="showDateSelect"
+				>
+					<uv-input
+						v-model="model.date"
+						border="none"
+						readonly
+						placeholder="请选择预约时间"
+					>
+					</uv-input>
+					<template v-slot:right>
+						<uv-icon name="arrow-right"></uv-icon>
+					</template>
+				</uv-form-item>
 			</uv-form>
 		</view>
 		<view class="footer" :style="{ height: `${paddingBottom}px` }">
@@ -84,17 +102,43 @@
 			title="请选择科室"
 			@select="handleSelectService"
 		/>
+		<uv-datetime-picker
+			ref="datetimePicker"
+			v-model="model.date"
+			mode="datetime"
+			:maxDate="maxDate"
+			:minDate="minDate"
+			:filter="filterDate"
+			@confirm="confirm"
+		/>
 	</view>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue';
 import { isEmpty } from '@/utils';
 
+// 一周后的日期
+const getSevenDayLater = () => {
+	const now = new Date();
+	now.setHours(23, 59, 59, 999);
+	const sevenDay = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
+	return sevenDay
+}
+// 明天凌晨
+const getTomorrow = () => {
+	const today = new Date();
+	today.setHours(0, 0, 0, 0);
+	// 添加一天
+	const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000)
+	return tomorrow
+}
+
 const paddingBottom = ref(60);
 const model = ref({
 	hospital: '',
 	department: '',
 	service: '',
+	date: '',
 });
 const rules = ref({
 	hospital: [{ required: true, message: '请选择医院' }],
@@ -104,10 +148,16 @@ const rules = ref({
 const hospitalSelect = ref();
 const departmentSelect = ref();
 const serviceSelect = ref();
+const datetimePicker = ref();
 
 const hospitalList = ref([{ name: '第一人民医院' }, { name: '第二人民医院' }]);
 const departmentList = ref([{ name: '内科' }, { name: '体检科' }]);
 const serviceList = ref([{ name: '体检' }]);
+
+// 日期弹窗组件最大日期
+const maxDate = ref(+new Date(getSevenDayLater()));
+// 最小日期
+const minDate = ref(+new Date(getTomorrow()));
 
 const showSelect = () => {
 	hospitalSelect.value?.open();
@@ -132,6 +182,25 @@ const showServiceSelect = () => {
 const handleSelectService = (e) => {
 	model.value.service = e.name;
 };
+
+// 过滤时间
+const filterDate = (type, options) => {
+	// console.log('type', type, options)
+	if (type === 'hour') {
+		return options.filter((option) => option > 8 && option < 14)
+	} else if (type === 'minute') {
+		return options.filter((option) => option % 30 === 0)
+	}
+	return options
+}
+
+const showDateSelect = () => {
+	
+
+	datetimePicker.value?.open();
+};
+// 时间确认
+const confirm = () => {};
 
 onMounted(() => {
 	const sysInfo = uni.getWindowInfo();
